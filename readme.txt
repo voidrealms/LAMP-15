78eddceea676ca9d9a73c5604e2a0e55a4b87b09
Bryan Cairns <bcairns@voidrealms.com>
2017-11-02 15:26:18 -0400

    First commit

---
+++ readme.txt
@@ -0,0 +1,10 @@
+Code for video tutorial
+
+Title: LAMP 15 - Array Primer
+Published: 11/3/2012 1:57:32 PM
+Link: https://www.youtube.com/watch?v=_90Y7ZPzKLk
+
+Description: A quick primer on arrays
+
+If you found this helpful, please consider donating: http://www.voidrealms.com
+
---
+++ tut15.php
@@ -0,0 +1,22 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
+<html>
+<head>
+	<title>Untitled</title>
+</head>
+
+<body>
+
+<?php
+
+$people = array("Bryan", "Eric", "Sally");
+
+echo $people[1] . "<br>";
+
+$people[1] = "Bob";
+
+echo $people[1] . "<br>";
+
+?>
+
+</body>
+</html>
